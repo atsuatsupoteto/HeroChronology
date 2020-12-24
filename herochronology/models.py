a7aa74732549ec11f42053a0626de70a5272dbdd
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2020-12-24 12:50:15 +0900

    ADD MODEL Hero_category,Hero_info

--- herochronology/models.py
+++ herochronology/models.py
@@ -3,8 +3,11 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-class Post(models.Model):
-    start_year = models.DateTimeField()
-    category = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Hero_category(models.Model):
+    category_name = models.CharaField()
+
+class Hero_info(models.Model):
+    start_year = models.DateField()
+    category = models.ForeignKey(Hero_category, on_delete=models.CASCADE)
     title = models.CharField()
     create_date = models.DateTimeField(default=timezone.now)
