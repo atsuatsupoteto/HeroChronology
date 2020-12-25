08697f4c5fef5549fcb825857a5f683c1059cd92
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2020-12-25 21:13:53 +0900

    create table Hero_category , Hero_info

--- herochronology/admin.py
+++ herochronology/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Hero_category
+from .models import Hero_info
 # Register your models here.
+
+admin.site.register(Hero_category)
+admin.site.register(Hero_info)
--- herochronology/models.py
+++ herochronology/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 # Create your models here.
 
 class Hero_category(models.Model):
-    category_name = models.CharField()
+    category_name = models.CharField(max_length=30)
 
 class Hero_info(models.Model):
     start_year = models.DateField()
     category = models.ForeignKey(Hero_category, on_delete=models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     create_date = models.DateTimeField(default=timezone.now)
